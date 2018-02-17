74b59377cc6baff0370b4d08d20e9d6c724a3dca
Adam Sheets <adamsheets@Adams-MacBook-Pro.local>
2018-02-16 20:44:15 -0600

    html update

--- Assets/javascript/project.js
+++ Assets/javascript/project.js
@@ -1,19 +1,4 @@
-<<<<<<< HEAD
-var zipcode = $(this).attr("postal-code"); 
-console.log(zipcode);
-
-var queryURL = "https://api.schooldigger.com/v1.1/schools?st=il&level=Elementary&zip=" + zipcode + "&nearLatitude=20&boundaryAddress=1700%20Hinman%20Ave&perPage=20&sortBy=rank&appID=9015e2b3&appKey=98c6ebf7ffad8d35ee898e50e0f69eab";
- 
-// Creating an AJAX call for the specific movie genre button being clicked
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-    console.log(response);
-});
-=======
-  
-  // Initialize Firebase
+    // Initialize Firebase
   var config = {
     apiKey: "AIzaSyDJy6cU-2ytgAE8bEXNYyRghC2BQefJ3YM",
     authDomain: "schoolzone-f4eb7.firebaseapp.com",
@@ -39,7 +24,7 @@ $.ajax({
 
     console.log(queryURL);
 
-    window.location.href = ".html"
+   
 
     //on click navigates to new page with results for the given search
 
@@ -48,4 +33,3 @@ $.ajax({
         method: "GET"
       })
     })
->>>>>>> 40288ca0f700724a2f1936f37d1d18f8bb889e79
