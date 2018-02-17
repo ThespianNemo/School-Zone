c6b43d9ea2ab5870a7a70e99e2c3cbfe99c4cfc3
Adam Sheets <adamsheets@Adams-MacBook-Pro.local>
2018-02-17 13:59:33 -0600

    working real API key

--- Assets/javascript/project.js
+++ Assets/javascript/project.js
@@ -20,24 +20,39 @@ $("#submit-info").on("click", function (event) {
   //grabs zip code user entered
   var userZip = $("#postal-code").val().trim();
   console.log("zip code: " + userZip);
+  // location.href = "results.html";
 
-
-//use geo code api to get state from zip code
+  //use geo code api to get state from zip code
   var getStateUrl = "http://maps.googleapis.com/maps/api/geocode/json?address=" + userZip + "&sensor=true";
 
-//ajax function to get 
+  //ajax function to get corresponding state
   $.ajax({
     url: getStateUrl,
     method: "GET"
   }).then(function (response) {
-    var result = response.results[0].address_components[3].short_name;
-    console.log("state: " + result);
+    var stateResult = response.results[0].address_components[3].short_name;
+    console.log("state: " + stateResult);
 
-    var queryURL = "https://api.schooldigger.com/v1.1/schools?st=" + result + "&zip=" + userZip + "&appID=9015e2b3&appKey=98c6ebf7ffad8d35ee898e50e0f69eab";
-
+    var queryURL = "https://api.schooldigger.com/v1.1/schools?st=" + stateResult + "&zip=" + userZip + "&appID=3d9ff2e4&appKey=cf32743f4707e77808f66d4cbc553e80";
     console.log("schooldigger: " + queryURL);
-
   })
-});
+
+//ajax function to get search results for the given zip code 
+  // $.ajax({
+  //   url: queryURL,
+  //   method: 'GET',
+  // }).then(function (response) {
+  //   var searchResults = response.response.docs
+  //   console.log("search: " + searchResults);
+
+  //   for (var i = 0; i < result.length; i++) {
+  //     var resultsDiv = $("<div class='item'>");
+  //     var headLine = result[i].headline.main;
+  //     var h1 = $("<p>").text(headLine);
+  //     resultsDiv.prepend(h1)
+  //     $("#results-here").prepend(resultsDiv);
+  //   }
+
+  });
 
 
