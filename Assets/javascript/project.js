c02bb16bde170241f982a1d26f08c5b90841313f
Adam Sheets <adamsheets@Adams-MacBook-Pro.local>
2018-02-19 13:29:25 -0600

    added if else so that correct state info is always pulled from geocode

--- Assets/javascript/project.js
+++ Assets/javascript/project.js
@@ -36,8 +36,23 @@ $("#submit-info").on("click", function (event) {
     url: getStateUrl,
     method: "GET"
   }).then(function (response) {
-    var stateResult = response.results[0].address_components[3].short_name;
+    var stateResult = "";
+
+    //check for State info in object
+    if (response.results[0].address_components[2].types[0] === "administrative_area_level_1") {
+      stateResult = response.results[0].address_components[2].short_name;
+    } else if (response.results[0].address_components[3].types[0] === "administrative_area_level_1") {
+      stateResult = response.results[0].address_components[3].short_name;
+    } else if (response.results[0].address_components[4].types[0] === "administrative_area_level_1") {
+      stateResult = response.results[0].address_components[4].short_name;
+    } else {
+
+    };
+
     console.log("state: " + stateResult);
+
+    // var test = response.results[0].address_components[3].types[0];
+    // console.log(test);
 
     var queryURL = "https://api.schooldigger.com/v1.1/schools?st=" + stateResult + "&zip=" + userZip + "&appID=3d9ff2e4&appKey=cf32743f4707e77808f66d4cbc553e80";
 
@@ -69,24 +84,19 @@ $("#submit-info").on("click", function (event) {
         var magnet = searchResults[i].isMagnetSchool;
         var isPrivate = searchResults[i].isPrivate;
         var avgScore = "";
-        
-        if (searchResults[i].rankHistory === null){
+
+        if (searchResults[i].rankHistory === null) {
           avgScore = "N/A"
         } else {
           avgScore = searchResults[i].rankHistory[0].averageStandardScore;
         };
-        
+
         var studentSize = searchResults[i].schoolYearlyDetails[0].numberOfStudents;
         var ratio = searchResults[i].schoolYearlyDetails[0].pupilTeacherRatio;
-        // console.log("charter? " + charter);
-        // console.log("magnet? :" + magnet);
-        // console.log("is private? :" + isPrivate);
-        // console.log("avg score: " + avgScore)
-
 
         //and unique ID to each item in results
         var ID = i + 1;
-        //add unique ID to each item in results
+        
         var table = $("<tr>");
         table.attr('id', ID);
 
@@ -97,12 +107,7 @@ $("#submit-info").on("click", function (event) {
 
         // Add table to the HTML
         $("#results-go-here > tbody").append(table);
-
-        // $("#1").on("click", function (event) {
-
-
-
-        // });
+        
       };
     });
   });
