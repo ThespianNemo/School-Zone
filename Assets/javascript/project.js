247ae21a5b167628e2b6ec85828a8886e14d7909
Adam Sheets <adamsheets@Adams-MacBook-Pro.local>
2018-02-20 20:27:23 -0600

    cleaned up logic for the extended results section

--- Assets/javascript/project.js
+++ Assets/javascript/project.js
@@ -1,3 +1,4 @@
+
 // Initialize Firebase
 var config = {
   apiKey: "AIzaSyDJy6cU-2ytgAE8bEXNYyRghC2BQefJ3YM",
@@ -26,19 +27,14 @@ function initMap() {
   });
 }
 
-
-//when user clicks "go" run this function
 $("#submit-info").on("click", function (event) {
-
   event.preventDefault();
-
   //removes search box upon results loading
   $(".first-row").hide();
   //shows the class which default is hidden on load
   $("#map").show();
   $(".second-row").show();
   $(".third-row").show();
-
 
   //grabs zip code user entered
   var userZip = $("#postal-code").val().trim();
@@ -69,9 +65,8 @@ $("#submit-info").on("click", function (event) {
       url: queryURL,
       method: 'GET',
     }).then(function (response) {
-      console.log(response);
+
       var searchResults = response.schoolList;
-      console.log(searchResults[2]);
 
       for (var i = 0; i < searchResults.length; i++) {
 
@@ -86,23 +81,8 @@ $("#submit-info").on("click", function (event) {
           stateRank = searchResults[i].rankHistory[0].rankStatewidePercentage + " %";
         }
 
-        //variables that will be needed for expanded results
-        var charter = searchResults[i].isCharterSchool;
-        var magnet = searchResults[i].isMagnetSchool;
-        var isPrivate = searchResults[i].isPrivate;
-        var avgScore = "";
-
-        if (searchResults[i].rankHistory === null) {
-          avgScore = "N/A"
-        } else {
-          avgScore = searchResults[i].rankHistory[0].averageStandardScore;
-        };
-
-        var studentSize = searchResults[i].schoolYearlyDetails[0].numberOfStudents;
-        var ratio = searchResults[i].schoolYearlyDetails[0].pupilTeacherRatio;
-  
         //and unique ID to each item in results
-        var ID = i + 1;
+        var ID = i;
         var table = $("<tr>");
         table.attr('id', ID);
         //add class to each row
@@ -117,73 +97,56 @@ $("#submit-info").on("click", function (event) {
         // Add table to the HTML
         $("#results-go-here > tbody").append(table);
 
-        //Check to see which crystal the user chose and add that crystal's value to the user's total score
         $(".result").on("click", function () {
           var choice = ($(this).attr("id"));
-          var resultChoice = "";
 
           $(".fourth-row").show();
           $(".fifth-row").show();
           $(".second-row").hide();
           $(".third-row").hide();
 
-          if (choice === "1") {
-            resultChoice = 0
-          } else if (choice === "2") {
-            resultChoice = 1
-          } else if (choice === "3") {
-            resultChoice = 2
-          } else if (choice === "4") {
-            resultChoice = 3
-          } else if (choice === "5") {
-            resultChoice = 4
-          } else if (choice === "6") {
-            resultChoice = 5
-          } else if (choice === "7") {
-            resultChoice = 6
-          } else if (choice === "8") {
-            resultChoice = 7
-          } else if (choice === "9") {
-            resultChoice = 8
-          } else if (choice === "10") {
-            resultChoice = 9
-          } else if (choice === "11") {
-            resultChoice = 10
+          if (searchResults[choice].rankHistory === null) {
+            avgScore = "N/A";
+          } else {
+            avgScore = searchResults[choice].rankHistory[0].averageStandardScore;
+          };
+
+          if (searchResults[choice].rankHistory === null) {
+            stateRank = "N/A";
+          } else {
+            stateRank = searchResults[choice].rankHistory[0].rankStatewidePercentage + " %";
+          };
+
+          if (searchResults[choice].isPrivate === true) {
+            type = "Private";
+          } else if (searchResults[choice].isCharterSchool === "Yes" ) {
+            type = "Charter";
+          } else if (searchResults[choice].isMagnetSchool === "Yes" ) {
+            type = "Magnet";
+          } else { 
+            type = "Public";
           }
 
-          displayExtended();
-
-          function displayExtended (){
-
-            if (searchResults[resultChoice].rankHistory === null) {
-              avgScore = "N/A"
-            } else {
-              avgScore = searchResults[resultChoice].rankHistory[0].averageStandardScore;
-            };
-
-            if (searchResults[resultChoice].rankHistory === null) {
-              stateRank = "N/A"
-            } else {
-              stateRank = searchResults[resultChoice].rankHistory[0].rankStatewidePercentage + " %";
-            }
-
-            $("#school-name").html("School Name: " + searchResults[resultChoice].schoolName);
-            $("#address").html("Address: " + searchResults[resultChoice].address.street);
-            $("#level").html("Level: " + searchResults[resultChoice].schoolLevel);
-            $("#state-rank").html("State Rank: " + stateRank);
-            $("#charter").html("Charter?: " + searchResults[resultChoice].isCharterSchool);
-            $("#magnet").html("Magnet?: " + searchResults[resultChoice].isMagnetSchool);
-            $("#private").html("Private?: " + searchResults[resultChoice].isPrivate);
-            $("#avg-score").html("Average Score: " + avgScore);
-            $("#student-size").html("Student Population: " + searchResults[resultChoice].schoolYearlyDetails[0].numberOfStudents);
-            $("#ratio").html("Student to Teacher Ratio: " + searchResults[resultChoice].schoolYearlyDetails[0].pupilTeacherRatio);
+          if (searchResults[choice].schoolYearlyDetails[0].pupilTeacherRatio === null) {
+            ratio = "N/A";
+          } else {
+            ratio = searchResults[choice].schoolYearlyDetails[0].pupilTeacherRatio;
           };
+
+          $("#school-name").html(searchResults[choice].schoolName);
+          $("#address").html(searchResults[choice].address.street);
+          $("#level").html(searchResults[choice].schoolLevel + " School");
+          $("#state-rank").html("State Rank: " + stateRank);
+          $("#type").html("Type: " + type);
+          $("#avg-score").html("Average Score: " + avgScore);
+          $("#student-size").html("Student Population: " + searchResults[choice].schoolYearlyDetails[0].numberOfStudents);
+          $("#ratio").html("Student to Teacher Ratio: " + ratio);
+
         });
       };
     });
   });
 });
-
 
 //event handler to reload page for user to start search over
 $("#restart-search").on("click", function (event) {
@@ -193,6 +156,7 @@ $("#restart-search").on("click", function (event) {
 $("#start-over").on("click", function (event) {
   location.reload();
 });
+
 
 $("#go-back").on("click", function (event) {
 
@@ -204,6 +168,7 @@ $("#postal-code").keypress(function (e) {
     $("#submit-info").click();
   }
 })
+
 
 
 
