e5742ca33e72dd225f2bfdf172bff5973c6f0958
Adam Sheets <adamsheets89@gmail.com>
2018-02-22 18:31:30 -0600

    fixed issues with avgScore variable and district variable

--- Assets/javascript/project.js
+++ Assets/javascript/project.js
@@ -99,6 +99,12 @@ $(window).on("load", function () {
           var level = searchResults[i].schoolLevel;
           var stateRank = "";
 
+          if (searchResults[i].rankHistory === null) {
+            stateRank = "N/A"
+          } else {
+            stateRank = searchResults[i].rankHistory[0].rankStatewidePercentage + " %";
+          };
+
           // combine address results into a readable address street+city+state+maybe zipcode
           var address = street + " " + city + " " + state;
           // console.log(address); 
@@ -124,26 +130,14 @@ $(window).on("load", function () {
               icon: DEFAULT_ICON
             });
             markerCollection.push(marker);
-            //hover marker with school info
-
-            //create function to hide or show schoool marker based on which school is clicked
-
-            // if clicked show, if not hide
-
-
-          })
-
-          if (searchResults[i].rankHistory === null) {
-            stateRank = "N/A"
-          } else {
-            stateRank = searchResults[i].rankHistory[0].rankStatewidePercentage + " %";
-          }
+          
+          });
 
           //and unique ID to each item in results
           var ID = i;
           var table = $("<tr>");
           table.attr('id', ID);
-
+          //add a class for results 
           table.addClass("result");
 
           //add school's data into the table
@@ -155,6 +149,7 @@ $(window).on("load", function () {
           // Add table to the HTML
           $("#results-go-here > tbody").append(table);
 
+          //click function to record which school the user selected
           $(".result").on("click", function () {
             var choice = ($(this).attr("id"));
 
@@ -167,19 +162,30 @@ $(window).on("load", function () {
               } else {
                 markerCollection[i].setIcon(DEFAULT_ICON)
               }
-            }
+            };
 
             $(".fourth-row").show();
             $(".fifth-row").show();
             $(".second-row").hide();
             $(".third-row").hide();
 
+            //additional variables to be displayed on the extended results page
             var fullAddress = searchResults[choice].address.street + " " + searchResults[choice].address.city + " " + searchResults[choice].address.state + " " + searchResults[choice].address.zip + "-" + searchResults[choice].address.zip4;
+            var avgScore = "";
+            var type = "";
+            var contact = searchResults[choice].phone;
+            var ratio = "";
+            var district = "";
+            var africanAm = searchResults[choice].schoolYearlyDetails[0].percentofAfricanAmericanStudents + " %";
+            var caucasian = searchResults[choice].schoolYearlyDetails[0].percentofWhiteStudents + " %";
+            var hispanic = searchResults[choice].schoolYearlyDetails[0].percentofHispanicStudents + " %";
+            var asianAm = searchResults[choice].schoolYearlyDetails[0].percentofAsianStudents + " %";
+            var indianAm = searchResults[choice].schoolYearlyDetails[0].percentofIndianStudents + " %";
 
             if (searchResults[choice].rankHistory === null) {
               avgScore = "N/A";
             } else {
-              avgScore = searchResults[choice].rankHistory[0].averageStandardScore;
+              avgScore = searchResults[choice].rankHistory[0].averageStandardScore.toFixed(2);
             };
 
             if (searchResults[choice].rankHistory === null) {
@@ -207,9 +213,13 @@ $(window).on("load", function () {
               ratio = searchResults[choice].schoolYearlyDetails[0].pupilTeacherRatio;
             };
 
-            var contact = searchResults[choice].phone;
-            var district = searchResults[choice].district.districtName;
+            if (searchResults[choice].district === undefined) {
+              district = "";
+            } else {
+              district = searchResults[choice].district.districtName;
+            };
 
+            //add extended information variables to the page
             $("#school-name").html(searchResults[choice].schoolName);
             $("#full-address").html(fullAddress);
             $("#phone").html(contact);
@@ -217,25 +227,14 @@ $(window).on("load", function () {
             $("#level").html(searchResults[choice].schoolLevel + " School");
             $("#state-rank").html(stateRank);
             $("#type").html("Type: " + type);
-            $("#avg-score").html(avgScore.toFixed(2));
-            console.log(avgScore);
+            $("#avg-score").html(avgScore);
             $("#student-size").html(searchResults[choice].schoolYearlyDetails[0].numberOfStudents);
             $("#ratio").html(ratio);
-
-            //additional variables to display on right side of exteneded results page
-
-            var africanAm = searchResults[choice].schoolYearlyDetails[0].percentofAfricanAmericanStudents + " %";
-            var caucasian = searchResults[choice].schoolYearlyDetails[0].percentofWhiteStudents + " %";
-            var hispanic = searchResults[choice].schoolYearlyDetails[0].percentofHispanicStudents + " %";
-            var asianAm = searchResults[choice].schoolYearlyDetails[0].percentofAsianStudents + " %";
-            var indianAm = searchResults[choice].schoolYearlyDetails[0].percentofIndianStudents + " %";
-
             $("#caucasian").html("<br>" + caucasian);
             $("#african-american").html(africanAm);
             $("#hispanic").html(hispanic);
             $("#asian-american").html(asianAm);
             $("#indian-american").html(indianAm);
-
           });
         };
       });
@@ -247,11 +246,12 @@ $(window).on("load", function () {
     location.reload();
   });
 
+  //allows user to restart their search
   $("#start-over").on("click", function (event) {
     location.reload();
   });
 
-
+  //function for user to return to the full list of schools in zip code
   $("#go-back").on("click", function (event) {
     $(".second-row").show();
     $(".third-row").show();
